8065a8c3a58eb461a8319029b37fc71565a4b2c5
Alex Svitkin <asvitkin@aras.com>
2021-07-22 10:39:27 +0300

    Initial commit

---
+++ src/test/java/FirstTestCLass.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTestCLass {
+    @Test
+    public void googleTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.by/");
+        Thread.sleep(1000);
+        driver.close();
+    }
+}
